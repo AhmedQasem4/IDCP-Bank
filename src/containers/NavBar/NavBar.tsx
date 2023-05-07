67673222a2ff29fd776a4f541f3f8fa477da301e
AhmedQasem4 <abok2ssem14@gmail.com>
2023-05-07 11:35:27 +0200

    fixing navbar issues (navbar final result).

--- src/containers/NavBar/NavBar.tsx
+++ src/containers/NavBar/NavBar.tsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react';
-import { images } from '../../assets';
-import { NavMenu } from '../../components';
-import {RiMenuAddLine, RiCloseCircleFill } from 'react-icons/ri';
-import './NavBar.css';
-
+import React, { useState } from "react";
+import { images } from "../../assets";
+import { NavMenu } from "../../components";
+import { RiMenuAddLine, RiCloseCircleFill } from "react-icons/ri";
+import "./NavBar.css";
 
 const NavBar = () => {
-
   const [toggoleMenu, setToggoleMenu] = useState(false);
 
-
   return (
-    <nav className='app__navbar flex space__between section__padding'>
+    <nav className="app__navbar flex space__between section__padding">
       <div className="navbar__logo">
         <img src={images.logo} alt="" />
       </div>
-      <div className='navbar__menu-links_container'>
-      <NavMenu />
+      <div className="navbar__links_container">
+        <NavMenu />
       </div>
       <div className="navbar__button">
-        <button className='btn btn-primary'>Service</button>
+        <button className="btn btn-primary">Service</button>
       </div>
       <div className="navbar__mobile-menu_container">
-        {toggoleMenu && <div className="navbar__mobile-menu"> <NavMenu /></div>}
-        <div className="navbar__menu-icon">
-        {toggoleMenu ? 
-        <RiCloseCircleFill size={27} onClick={()=> setToggoleMenu(false)}/>:
-        <RiMenuAddLine size={27} onClick={()=> setToggoleMenu(true)}/>}
-        </div>
+        <RiMenuAddLine size={27} onClick={() => setToggoleMenu(true)} />
+        {toggoleMenu && (
+          <div className="navbar__mobile-menu flex flex__center">
+            <RiCloseCircleFill
+              className="close__icon"
+              size={27}
+              onClick={() => setToggoleMenu(false)}
+            />
+            <NavMenu />
+          </div>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
